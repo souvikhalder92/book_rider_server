e938005e021aea1334d5406c28cb43f8de219a92
souvikhalder92 <107093196+souvikhalder92@users.noreply.github.com>
2022-11-09 01:25:07 +0600

    ok

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,6 +21,7 @@ async function run(){
 
 
         const serviceCollection = client.db('bookRider').collection('services');
+        const reviewCollection = client.db('bookRider').collection('reviews');
        
 
     
@@ -37,9 +38,36 @@ async function run(){
             const services = await cursor.toArray();
             res.send(services);
 
+        
         });
-
+        app.get('/allservices/:id',async (req,res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+          
+        });
+        app.get('/reviews',async (req,res) =>{
+            let query = {};
+            if(req.query.email)
+            {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
     
+         })
+
+
+     app.post('/reviews',async (req,res) =>{
+        const review = req.body;
+        const result = await reviewCollection.insertOne(review);
+        res.send(result);
+
+     })
    
 
 
