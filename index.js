07981bdb047b720164776c10fe8b761c2e5d933e
souvikhalder92 <107093196+souvikhalder92@users.noreply.github.com>
2022-11-08 13:06:47 +0600

    ok

--- index.js
+++ index.js
@@ -15,11 +15,41 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fwyrt73.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    try{
+
+
+        const serviceCollection = client.db('bookRider').collection('services');
+       
+
+    
+        app.get('/services', async (req,res) =>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+
+        });
+        app.get('/allservices', async (req,res) =>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+
+        });
+
+    
+   
+
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(error => console.log(error));
 
 
 app.get('/',(req,res) =>{
