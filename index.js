7bba55ee0fe314ea91428d1f33c8dcaea129789f
souvikhalder92 <107093196+souvikhalder92@users.noreply.github.com>
2022-11-10 01:50:19 +0600

    ok

--- index.js
+++ index.js
@@ -17,22 +17,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-function verifyJWT(req, res, next){
-    const authHeader = req.headers.authorization;
 
-    if(!authHeader){
-        return res.status(401).send({message: 'unauthorized access'});
-    }
-    const token = authHeader.split(' ')[1];
-
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
-        if(err){
-            return res.status(403).send({message: 'Forbidden access'});
-        }
-        req.decoded = decoded;
-        next();
-    })
-}
 async function run(){
     try{
 
